cfa495c2adceaf50741cd340ff4ebf5cf1684229
John Murphy <John@TrueAnalyticsSolutions.com>
2023-03-10 10:57:28 -0500

    fixed plotting - added additional trained models

--- AICryptoBot/krm_lib/utilities/helpers.py
+++ AICryptoBot/krm_lib/utilities/helpers.py
@@ -12,10 +12,18 @@ class Plotters():
     def __init__(self):
         pass
     
-    def plot_learning_curve(self, x, scores, figure_file):
+    def plot_learning_curve(self, x, scores, figure_file="learning_curve.png"):
+        plt_learning_curve_fig = plt.figure(figsize=(15,5))
         running_avg = np.zeros(len(scores))
         for i in range(len(running_avg)):
             running_avg[i] = np.mean(scores[max(0, i-50):(i+1)])
             plt.plot(x, running_avg)
             plt.title('Running average of previous 50 scores')
             plt.savefig(figure_file)
+
+    def plot_market_train_test(self, df_train, df_test, figure_file="train_test_split.png"):
+        train_test_fig = plt.figure(figsize=(15,5))
+        df_train["Close_Price"].plot()
+        df_test["Close_Price"].plot()
+        plt.title('Market Price - Train/Test Split')
+        plt.savefig(figure_file)
